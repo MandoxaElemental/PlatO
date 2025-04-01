c84f0e37492b11d22a9f79da24c0b6e2e526ab90
Gianpaolo Reinares <gprcards@gmail.com>
2025-04-01 10:01:48 -0700

    Added the main layout template

--- my-app/src/app/Components/NavbarComponentLeft.tsx
+++ my-app/src/app/Components/NavbarComponentLeft.tsx
@@ -1,32 +1,64 @@
 
 "use client";
 
-import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from "flowbite-react";
-export function Component() {
+import { Button, Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from "flowbite-react";
+
+export function NavbarComponent() {
   return (
     <Sidebar aria-label="Default sidebar example">
       <SidebarItems>
+      <img src="./assets/4.svg" alt="plato"/>
         <SidebarItemGroup>
-          <SidebarItem href="/" icon={}>
-            Home
+          <SidebarItem href="/" >
+            <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/house.svg" alt="home" /><p className="pl-2">Home</p>
+            </div>
           </SidebarItem>
-          <SidebarItem href="/Discover" icon={} label="Pro" labelColor="dark">
-            Discover
+          <SidebarItem href="/Discover">
+          <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/search.svg" alt="discover" /><p className="pl-2">Discover</p>
+            </div>
           </SidebarItem>
-          <SidebarItem href="#" icon={} label="3">
-            Notificiations
+          <SidebarItem href="/Notifications">
+          <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/bell.svg" alt="notifications" /><p className="pl-2">Notifications</p>
+            </div>
           </SidebarItem>
-          <SidebarItem href="#" icon={}>
-            Premium
+          <SidebarItem href="/Premium">
+          <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/award.svg" alt="notifications" /><p className="pl-2">Premium</p>
+            </div>
           </SidebarItem>
-          <SidebarItem href="#" icon={}>
-            Messages
+          <SidebarItem href="/Messages" >
+          <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/envelope.svg" alt="messages" /><p className="pl-2">Messages</p>
+            </div>
           </SidebarItem>
-          <SidebarItem href="/RecipeBook" icon={}>
-            Recipe Book
+          <SidebarItem href="/RecipeBook" >
+          <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/book.svg" alt="book" /><p className="pl-2">Recipe Book</p>
+            </div>
           </SidebarItem>
-          <SidebarItem href="/Profile" icon={}>
-            Profile
+          <SidebarItem href="/Profile" >
+          <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/person.svg" alt="profile" /><p className="pl-2">Profile</p>
+            </div>
+          </SidebarItem>
+          <Button className="w-full text-center">Post</Button>
+        </SidebarItemGroup>
+        <SidebarItemGroup>
+          <SidebarItem href="#" >
+          <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/person.svg" alt="profile" /><p className="pl-2">UserName</p>
+            </div>
+          </SidebarItem>
+          <SidebarItem href="#" >
+          <div className="flex items-center">
+            <img className="h-5 w-5" src="./assets/gear.svg" alt="profile" /><p className="pl-2">Settings and Privacy</p>
+            </div>
+          </SidebarItem>
+          <SidebarItem href="#" >
+            Log Out
           </SidebarItem>
         </SidebarItemGroup>
       </SidebarItems>
