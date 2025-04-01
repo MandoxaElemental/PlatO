63c10509043a500a97060f359b55ac1198fc9820
Alexander <yoda4115@gmail.com>
2025-04-01 15:16:53 -0700

    login frontend - started implementing opening login portion

--- my-app/src/app/(pages)/Login/LoginPage.tsx
+++ my-app/src/app/(pages)/Login/LoginPage.tsx
@@ -1,10 +1,16 @@
+import LoginStartPage from '@/app/Components/LoginComponents/StartPage';
 import Image from 'next/image';
 
 const LoginPage = () => {
   return (
     <>
-        <div>
+        <div className='grid grid-cols-2'>
+          <div className='justify-items-center'>
             <Image src={`/images/Logo.png`} alt="logo" width={500} height={500}/>  
+          </div>
+          <div>
+            <LoginStartPage/>
+          </div>
         </div>
     </>
   )
