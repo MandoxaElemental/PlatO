14d6f13f55f55b1a2ea8e02bbe67c5e64ea8c87a
Gianpaolo Reinares <gprcards@gmail.com>
2025-04-02 15:48:49 -0700

    Saving to my Branch

--- my-app/src/app/(pages)/Discover/page.tsx
+++ my-app/src/app/(pages)/Discover/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Discover = () => {
   return (
-    <div>
+    <div className='pt-10 px-5 w-full'>
       <div className='border-b-1 border-solid border-slate-300 p-2 text-2xl font-semibold text-center'>
             Discover
         </div>
