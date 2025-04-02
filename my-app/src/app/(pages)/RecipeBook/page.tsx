1b8a24d25da2943319dda903f8f24aa8cb725051
Gianpaolo Reinares <gprcards@gmail.com>
2025-04-02 13:40:52 -0700

    Just Saving to My Branch

--- my-app/src/app/(pages)/RecipeBook/page.tsx
+++ my-app/src/app/(pages)/RecipeBook/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const RecipeBook = () => {
   return (
-    <div>RecipeBook</div>
+    <div>
+      <div className='border-b-1 border-solid border-slate-300 p-2 text-2xl font-semibold text-center'>
+            Recipe Book
+        </div>
+        <div className='grid grid-cols-2'>
+
+        </div>
+    </div>
   )
 }
 
